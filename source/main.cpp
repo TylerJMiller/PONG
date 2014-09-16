8e301d04bec32ab2fbd4140a257096f8aab58ac4
no bones about it <chronogene@gmail.com>
2014-09-16 10:39:15 -0700

    updating base

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,17 @@
 ﻿#include "AIE.h"
 #include <iostream>
+#include "MyMaths.h"
 
 int main( int argc, char* argv[] )
 {	
-    Initialise(800, 600, false, "My Awesome Game");
+    Initialise(800, 600, false, "poNg");
     
     SetBackgroundColour(SColour(0, 0, 0, 255));
 
-    int myTextureHandle = CreateSprite("./images/crate_sideup.png", 64, 64, true);
-    float xPos = 100;
-    float yPos = 100;
 
     //Game Loop
     do
     {
-        MoveSprite(myTextureHandle, xPos, yPos);
-        DrawSprite(myTextureHandle);
 
         ClearScreen();
 
